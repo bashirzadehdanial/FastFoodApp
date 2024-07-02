ef8c0f643d0ee1b110c47f51be943ae31d0f6741
Danial <bashirzadehdanial@gmail.com>
2024-07-02 16:37:59 +0100

    FastFoodApp is initialised

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FastFoodTerminal
+{
+   class program
+   {
+    static void Main(string[] args)
+    {
+        string[] menuItems = {"fries", "cheese", "burger", "fanta", "vegie", "burger", "kids meal"};
+        List<string> orderItems = new List<string>();
+        string customerName;
+        bool addingMoreItems = true;
+
+        
+    }
+   }
+}
