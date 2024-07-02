b9da37cbebd7d2c685eb1017bf34943468304a8f
Danial <bashirzadehdanial@gmail.com>
2024-07-02 16:39:23 +0100

    Menu functionalities are added

--- Program.cs
+++ Program.cs
@@ -12,7 +12,36 @@ namespace FastFoodTerminal
         string customerName;
         bool addingMoreItems = true;
 
-        
+        do
+        {
+            Console.WriteLine("Menu:");
+            for(int i = 0; i<menuItems.Length;i++)
+            {
+                Console.WriteLine($"{i + 1}.{menuItems[i]}");
+            }
+            Console.Write("Enter the number of the item you want to order:");
+            int choice;
+            while(!int.TryParse(Console.ReadLine(), out choice)|| choice<1|| choice > menuItems.Length)
+            {
+                Console.WriteLine("Invalid input. Please enter a number from the menue.");
+                Console.Write("Enter the number of the item you want to order:");
+            }
+            orderItems.Add(menuItems[choice - 1]);
+            Console.Write("Add another item? (Yes/No):");
+            string addMore = Console.ReadLine().Trim().ToLower();
+            addingMoreItems = (addMore == "yes");
+
+        }while(addingMoreItems);
+        {
+           Console.Write("Enter customer name for the order:");
+           customerName = Console.ReadLine();
+
+           Console.WriteLine($"\nOrder summery for {customerName}:");
+           foreach (var item in orderItems)
+           {
+            Console.WriteLine($"-{item}");
+           }
+        }
     }
    }
 }
